07293a8bd19d6d8cc2bd1c49138c4fcf0649a186
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2016-04-10 16:03:03 -0700

    working minified builds from src

--- src/app.js
+++ src/app.js
@@ -6,26 +6,23 @@ import 'ionic-sdk/release/js/ionic.bundle';
 
 import homeRouteModule from './routes/home/home.route';
 import profileRouteModule from './routes/profile/profile.route';
-// import runModule from './config/main.config';
+import runModule from './config/main.config';
 
-let mod = angular.module('app', [
+let mainModule = angular.module('app', [
   'ionic',
   homeRouteModule,
   profileRouteModule,
 ]);
 
 // Run
-mod.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
+mainModule.run(runModule);
+
+// Bootstrap
+angular.element(document).ready(function() {
+  angular.bootstrap(document.querySelector('[data-main-app]'), [
+    mainModule.name
+  ], {
+    strictDi: true
   });
 });
 
