881bf01259749fca47905c41458cfb0343c5842c
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2016-04-11 17:04:30 -0700

    big note

--- loaders/relative-loader.js
+++ loaders/relative-loader.js
@@ -1,10 +1,19 @@
+/**
+ * This is directly taken from https://github.com/webpack/file-loader, as a
+ * hack/fix to get ionic fonts in their proper path. Even though this is a direct copy
+ * of file-loader v 0.8.5, it doesn't have the query option so the fonts dir is hard
+ * coded. This was a response to needed a relative path for the `publicPath` in the
+ * webpack config, which all the ionic fonts were prefixed `assets/bundles/assets/bundles/fonts/[name].[ext]`.
+ *
+ * Using an absolute publicPath, e.g. `/assets/bundles/`, broke iOS builds (and probably android too). Im guessing
+ * because the `www` directory is copied to the various platform dirs and the web app is served from a parent dir
+ * and not www.
+ */
+
 /*
   MIT License http://www.opensource.org/licenses/mit-license.php
   Author Tobias Koppers @sokra
 */
-
-// this is directly taken from https://github.com/webpack/file-loader
-// and is a hack that somehow loses the `query` and sets the proper path.
 
 var loaderUtils = require("loader-utils");
 
