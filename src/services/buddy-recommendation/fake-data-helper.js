4224b6ffb3f3a43bcf8fb9b9080462901d8758dc
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2016-04-12 16:10:30 -0700

    small refactor

--- src/services/buddy-recommendation/fake-data-helper.js
+++ src/services/buddy-recommendation/fake-data-helper.js
@@ -1,19 +1,18 @@
 /**
  * handles loading names.json
  */
-import shuffle from "lodash/shuffle";
 import sample from "lodash/sample";
-import random from "lodash/random";
+import sampleSize from "lodash/sampleSize";
 
 const MAX_NUMBER_OF_PROFILES = 100;
 
 export function processNames(data) {
-    return shuffle(data.names).slice(0, MAX_NUMBER_OF_PROFILES);
+    return sampleSize(data.names, MAX_NUMBER_OF_PROFILES);
 }
 
 export function processProfiles(data) {
     data.pics = data.pics || [];
-    data.pics = shuffle(data.pics).slice(0, MAX_NUMBER_OF_PROFILES);
+    data.pics = sampleSize(data.pics, MAX_NUMBER_OF_PROFILES);
     return data.pics.map((s3link) => {
         return {
             profile_pic_tiny: s3link,
