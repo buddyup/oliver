b11a241a1b85a40bb7456420fcac65f634cdf3f5
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2016-04-15 15:26:38 -0700

    try looping through it

--- src/routes/students/cards/students.controller.js
+++ src/routes/students/cards/students.controller.js
@@ -7,13 +7,20 @@ mod.controller('studentsController', [
   'buddyRecommendationService',
   'buddyDetailIndex',
   'studentIds',
-  function($scope, buddyRecommendationService, buddyDetailIndex, studentIds) {
+  '$interval',
+  function($scope, buddyRecommendationService, buddyDetailIndex, studentIds, $interval) {
     if (typeof buddyDetailIndex === 'undefined' || buddyDetailIndex === -1) {
       buddyDetailIndex = 0;
     }
     $scope.options = {
-      loop: false,
-      initialSlide: buddyDetailIndex,
+      // ALECK CHANGES
+      // loop: false,
+      initialSlide: 1,
+      loop: true,
+      loopAdditionalSlides: 0,
+      loopedSlides: 0,
+      slidesPerView: 1,
+      // END ALECK CHANGES
       effect: "slide",
       touchMoveStopPropagation: true,
       paginationBulletRender: function (index, className) { return ''; },
@@ -22,17 +29,88 @@ mod.controller('studentsController', [
       parallax: false,
       paginationCurrentClass: "hidden",
       // handle large lists
-      preloadImages: false,
-      lazyLoading: true,
-      lazyLoadingInPrevNext: true,
-      lazyLoadingOnTransitionStart: true,
+      // preloadImages: false,
+      // lazyLoading: true,
+      // lazyLoadingInPrevNext: true,
+      // lazyLoadingOnTransitionStart: true,
+      onSlideChangeStart: handleSlideChangeStart,
     };
+
+    let sliceStart, sliceEnd, previousActiveIndex = 4, currentCard = 'Middle';
+    sliceStart = buddyDetailIndex === 0 ? studentIds.length - 1 : buddyDetailIndex - 1;
+    sliceEnd = buddyDetailIndex === studentIds.length - 1 ? 0 : buddyDetailIndex + 1;
+
+    function cardLeft(currentCard) {
+      if (currentCard === 'Middle') {
+        return 'Start';
+      }
+      if (currentCard === 'Start') {
+        return 'End';
+      }
+      if (currentCard === 'End') {
+        return 'Middle';
+      }
+    }
+    function cardRight(currentCard) {
+      if (currentCard === 'Middle') {
+        return 'End';
+      }
+      if (currentCard === 'Start') {
+        return 'Middle';
+      }
+      if (currentCard === 'End') {
+        return 'Start';
+      }
+    }
+
+    /**
+     * handle fetching new cards, even though there are only 3 cards, the swiper.activeIndex will hit 0 - 4.
+     */
+    function handleSlideChangeStart (swiper) {
+      let currentIndex, updateCardIndex;
+      console.log(swiper.activeIndex, previousActiveIndex);
+      // this starts at an activeIndex == 4 and in the middle
+      if (previousActiveIndex === swiper.activeIndex) {
+        // do nothing
+        console.log('do nothing')
+      } else if ((swiper.activeIndex < previousActiveIndex || swiper.activeIndex === previousActiveIndex + 2) && swiper.activeIndex !== previousActiveIndex - 2)  {
+        // user swiped left, update the next left card
+        currentCard = cardLeft(currentCard);
+        updateCardIndex = `card${cardLeft(currentCard)}Index`;
+        currentIndex = $scope.data[`card${currentCard}Index`];
+        $scope.data[updateCardIndex] = currentIndex === 0 ? studentIds.length - 1 : currentIndex - 1;
+        console.log('left', currentCard, updateCardIndex, currentIndex)
+      } else {
+        // user swiped right, update the next right card
+        currentCard = cardRight(currentCard);
+        updateCardIndex = `card${cardRight(currentCard)}Index`;
+        currentIndex = $scope.data[`card${currentCard}Index`];
+        $scope.data[updateCardIndex] = currentIndex === studentIds.length - 1 ? 0 : currentIndex + 1;
+        console.log('right', currentCard, updateCardIndex, currentIndex)
+      }
+
+      previousActiveIndex = swiper.activeIndex;
+    }
+    let testA = true;
+    $interval( _ => {
+      if (testA) {
+        $scope.data.aleck_test = 'true';
+      } else {
+        $scope.data.aleck_test = 'false';
+      }
+      testA = !testA;
+    }, 1000);
 
     $scope.data = {
       brs: buddyRecommendationService,
       studentIds: studentIds,
       studentMap: buddyRecommendationService.studentMap,
+      cardStartIndex: sliceStart,
+      cardEndIndex: sliceEnd,
+      cardMiddleIndex: buddyDetailIndex,
+      aleck_test: 'argarg',
     };
+
 }]);
 
 export default mod = mod.name;
