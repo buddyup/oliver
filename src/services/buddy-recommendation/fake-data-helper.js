d7e2210a32b108a72946932acb7cdbb689acb09c
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2016-04-19 12:05:06 -0700

    some repeatable fake ids

--- src/services/buddy-recommendation/fake-data-helper.js
+++ src/services/buddy-recommendation/fake-data-helper.js
@@ -45,23 +45,21 @@ export function addRanking(buddy) {
     return buddy;
 }
 
-function generateUUID(){
-    var d = new Date().getTime();
-    if(window.performance && typeof window.performance.now === "function"){
-        d += performance.now(); //use high-precision timer if available
+function generateIDfn() {
+    let idSeed = 10;
+
+    return function generateID(){
+        idSeed = idSeed + 1;
+        return `-KBogDiHDlb0ZwD-A${idSeed}`;
     }
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (d + Math.random()*16)%16 | 0;
-        d = Math.floor(d/16);
-        return (c==='x' ? r : (r&0x3|0x8)).toString(16);
-    });
-    return uuid;
 }
+
+const generateID = generateIDfn();
 
 /**
  * I think fire base puts the id as $id
  */
 export function addId(buddy) {
-    buddy.$id = generateUUID();
+    buddy.$id = generateID();
     return buddy;
 }
