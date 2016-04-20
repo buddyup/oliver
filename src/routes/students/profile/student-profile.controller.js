cffb0435020b19c363076be7cc19a5bb0b09d6c7
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2016-04-20 14:24:47 -0700

    buddy up

--- src/routes/students/profile/student-profile.controller.js
+++ src/routes/students/profile/student-profile.controller.js
@@ -1,16 +1,30 @@
 import buddyRecommendationServiceModule from 'services/buddy-recommendation/buddy-recommendation.service';
+import has from "lodash/has";
 
 let mod = angular.module('studentProfileControllerModule', [buddyRecommendationServiceModule]);
 
 mod.controller('studentProfileController', [
   'buddyRecommendationService',
   'student',
-  function(buddyRecommendationService, student) {
+  'profile',
+  function(buddyRecommendationService, student, profile) {
     const ctrl = this;
+
+    /**
+     * makes a buddy request and updates view
+     */
+    function handleBuddyUpClick() {
+        profile.buddies[student.id] = student;
+        profile.buddiesAsArray.push(student);
+        ctrl.isBuddy = true;
+    }
 
     angular.extend(ctrl, {
       student: student,
       brs: buddyRecommendationService,
+      profile: profile,
+      isBuddy: has(profile.buddies, student.id),
+      handleBuddyUpClick: handleBuddyUpClick,
     });
 
 }]);
